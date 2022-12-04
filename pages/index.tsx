228d4ed59a3ba450966ded488c6962dc77f0d273
Grant Kyle <sgrantkyle@gmail.com>
2022-12-04 16:55:14 -0600

    first commit with dependencies installed including tailwind, sass and graphql, dummy post data

---
+++ pages/index.tsx
@@ -0,0 +1,46 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const posts: { title: string; excerpt: string }[] = [
+  { title: "This is a test title", excerpt: "This is an excerpt for our blog" },
+  {
+    title: "This is the second test title",
+    excerpt: "This is the second excerpt for our blog",
+  },
+  {
+    title: "This is the third test title",
+    excerpt: "This is the third excerpt for our blog",
+  },
+];
+
+const Home: NextPage = () => {
+  return (
+    <div className="container mx-auto px-10 mb-8 bg-gray-300">
+      <Head>
+        <title>Grant Kyle's Personal Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <div>
+              <div>
+                <p>
+                  <b>{post.title}</b>
+                </p>
+                {post.excerpt}{" "}
+              </div>
+              <br />
+            </div>
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
