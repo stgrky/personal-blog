85bc6248a26f75aacf2d254b57f1c931cc359cff
Grant Kyle <sgrantkyle@gmail.com>
2023-01-12 14:48:49 -0600

    layout surrounding app

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Header from "../Header.jsx";
 
-type Props = {
+type LayoutProps = {
   children: JSX.Element;
 };
 
-const Layout = ({ children }: Props) => {
+const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <Header />
