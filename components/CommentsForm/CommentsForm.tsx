b065067aa823a85c8b7a88e2f7b5a98e66d4ed84
Steven Kyle <sgrantkyle@gmail.com>
2023-05-09 19:38:59 -0600

    begin comment area

--- components/CommentsForm/CommentsForm.tsx
+++ components/CommentsForm/CommentsForm.tsx
@@ -1,9 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { NextPage } from "next";
 
-const CommentsForm = ({ slug }) => {
+interface CommentsFormWidgets {
+  slug: any;
+}
+
+const CommentsForm: NextPage<CommentsFormWidgets> = ({ slug }) => {
   const [error, setError] = useState(false);
-  const [localStorage, setLocalStorage] = useState(second)
-  return <div>CommentsForm</div>;
+  const [localStorage, setLocalStorage] = useState(null);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  const commentElement = useRef();
+  const nameElement = useRef();
+  const emailElement = useRef();
+  const storeDataElement = useRef();
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4"></h3>
+      <div className="grid grid-cols-1 gap-4 mb-4"></div>
+      <div className="grid grid-cols-1 gap-4 mb-4"></div>
+      <div className="grid grid-cols-1 gap-4 mb-4"></div>
+    </div>
+  );
 };
 
 export default CommentsForm;
